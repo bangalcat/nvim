750d7b847a3e3345a8ab8de235e323fcb31c3807
Donghyun <semaj9310@gmail.com>
2023-10-01 23:05:55 +0900

    remove nvim-ufo
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,11 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-c>", "<cmd>normal! ciw<cr>a")
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
