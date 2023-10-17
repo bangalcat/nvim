44c17ab69df45334a02643c1dd769d15d552f360
Donghyun <semaj9310@gmail.com>
2023-10-17 23:37:54 +0900

    update keymaps
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,18 +4,35 @@
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "h", "gh")
+vim.keymap.set("n", "k", "gk")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "<leader>sq", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("x", "<leader>cw", [[y<cmd>let @/=substitute(escape(@", '/'), '\n', '\\n', 'g')<cr>"_cgn]])
-vim.keymap.set("n", "<leader>cw", [[<cmd>let @/='\<'.expand('<cword>').'\>'<CR>"_ciw]])
+vim.keymap.set(
+  "n",
+  "<leader>sq",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Replace current word in file" }
+)
+vim.keymap.set(
+  "x",
+  "<leader>cw",
+  [[y<cmd>let @/=substitute(escape(@", '/'), '\n', '\\n', 'g')<cr>"_cgn]],
+  { desc = "Replace current word" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>cw",
+  [[<cmd>let @/='\<'.expand('<cword>').'\>'<CR>"_ciw]],
+  { desc = "Replace current word" }
+)
 vim.keymap.set({ "n" }, "<leader>fs", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- my own keymaps
 vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("n", "<Tab>", "")
-vim.keymap.set("n", "<A-i>", "<C-i>", { noremap = false })
+-- vim.keymap.set("n", "<Tab>", "")
+-- vim.keymap.set("n", "<A-i>", "<C-i>", { noremap = false })
